d13fdb6bbefbe01acfb61340828b0a3deb024451
Bogdan <56641179+Bogdanselect@users.noreply.github.com>
2021-12-17 15:46:21 +0300

    Add files via upload

---
+++ js/script_Madal_X1.js
@@ -0,0 +1,159 @@
+// Модальное окно 1
+var modal = document.getElementById('myModal1');
+var btn = document.getElementById("myBtn_model_F1");
+var span = document.getElementsByClassName("close_model_F1")[0];
+
+btn.onclick = function (){
+    modal.style.display = "block";
+}
+
+span.onclick = function(){
+    modal.style.display = "none";
+}
+
+modal.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+}
+//
+
+
+
+// Модальное окно 2
+var modal2 = document.getElementById('myModal2');
+var btn2 = document.getElementById("myBtn_model_F2");
+var span2 = document.getElementsByClassName("close_model_F2")[0];
+
+btn2.onclick = function (){
+    modal2.style.display = "block";
+}
+
+span2.onclick = function(){
+    modal2.style.display = "none";
+}
+
+modal2.onclick = function(event){
+    if(event.target == modal2){
+        modal2.style.display = "none";
+    }
+}
+//
+// Модальное окно 3
+var modal3 = document.getElementById('myModal3');
+var btn3 = document.getElementById("myBtn_model_F3");
+var span3 = document.getElementsByClassName("close_model_F3")[0];
+
+btn3.onclick = function (){
+    modal3.style.display = "block";
+}
+
+span3.onclick = function(){
+    modal3.style.display = "none";
+}
+
+modal3.onclick = function(event){
+    if(event.target == modal3){
+        modal3.style.display = "none";
+    }
+}
+//
+// Модальное окно 4
+var modal4 = document.getElementById('myModal4');
+var btn4 = document.getElementById("myBtn_model_F4");
+var span4 = document.getElementsByClassName("close_model_F4")[0];
+
+btn4.onclick = function (){
+    modal4.style.display = "block";
+}
+
+span4.onclick = function(){
+    modal4.style.display = "none";
+}
+
+modal4.onclick = function(event){
+    if(event.target == modal4){
+        modal4.style.display = "none";
+    }
+}
+//
+// Модальное окно 5
+var modal5 = document.getElementById('myModal5');
+var btn5 = document.getElementById("myBtn_model_F5");
+var span5 = document.getElementsByClassName("close_model_F5")[0];
+
+btn5.onclick = function (){
+    modal5.style.display = "block";
+}
+
+span5.onclick = function(){
+    modal5.style.display = "none";
+}
+
+modal5.onclick = function(event){
+    if(event.target == modal5){
+        modal5.style.display = "none";
+    }
+}
+//
+
+
+// Модальное окно 6
+var modal6 = document.getElementById('myModal6');
+var btn6 = document.getElementById("myBtn_model_F6");
+var span6 = document.getElementsByClassName("close_model_F6")[0];
+
+btn6.onclick = function (){
+    modal6.style.display = "block";
+}
+
+span6.onclick = function(){
+    modal6.style.display = "none";
+}
+
+modal6.onclick = function(event){
+    if(event.target == modal6){
+        modal6.style.display = "none";
+    }
+}
+//
+
+
+// Модальное окно 7
+var modal7 = document.getElementById('myModal7');
+var btn7 = document.getElementById("myBtn_model_F7");
+var span7 = document.getElementsByClassName("close_model_F7")[0];
+
+btn7.onclick = function (){
+    modal7.style.display = "block";
+}
+
+span7.onclick = function(){
+    modal7.style.display = "none";
+}
+
+modal7.onclick = function(event){
+    if(event.target == modal7){
+        modal7.style.display = "none";
+    }
+}
+//
+// Модальное окно 8
+var modal8 = document.getElementById('myModal8');
+var btn8 = document.getElementById("myBtn_model_F8");
+var span8 = document.getElementsByClassName("close_model_F8")[0];
+
+btn8.onclick = function (){
+    modal8.style.display = "block";
+}
+
+span8.onclick = function(){
+    modal8.style.display = "none";
+}
+
+modal8.onclick = function(event){
+    if(event.target == modal8){
+        modal8.style.display = "none";
+    }
+}
+//
